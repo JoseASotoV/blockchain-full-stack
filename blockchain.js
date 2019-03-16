e08809171133421400469be537319b6dcd817c74
JaSv <orcwarrior_9@hotmail.com>
2019-03-15 23:57:35 -0700

    adding blockchain support

---
+++ blockchain.js
@@ -0,0 +1,53 @@
+const Block = require("./block");
+const cryptoHash = require("./crypto-hash");
+
+class Blockchain {
+  constructor() {
+    this.chain = [Block.genesis()];
+  }
+
+  addBlock({ data }) {
+    const newBlock = Block.mineBlock({
+      lastBlock: this.chain[this.chain.length - 1],
+      data
+    });
+
+    this.chain.push(newBlock);
+  }
+
+  replaceChain(chain) {
+    if (chain.length <= this.chain.length) {
+      console.error("the incoming chain must be longer");
+      return;
+    }
+
+    if (!Blockchain.isValidChain(chain)) {
+      console.error("the incoming chain must be valid");
+      return;
+    }
+
+    console.log("replacing chain with ", chain);
+    this.chain = chain;
+  }
+
+  static isValidChain(chain) {
+    if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) {
+      return false;
+    }
+
+    for (let i = 1; i < chain.length - 1; i++) {
+      const { timestamp, lastHash, hash, data } = chain[i];
+      const lastBlockHash = chain[i - 1].hash;
+
+      if (lastHash !== lastBlockHash) return false;
+
+      const validatedHash = cryptoHash(timestamp, lastHash, data);
+
+      if (hash !== validatedHash) return false;
+    }
+
+    return true;
+  }
+}
+
+module.exports = Blockchain;
